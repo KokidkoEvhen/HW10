dfc9bb045c2b9aa388ad1a040e955283f6c1b5bf
evhen <e3k8o9k8>
2021-10-01 19:10:21 +0300

    Automated creating data, random data assigning

--- public/index.php
+++ public/index.php
@@ -7,119 +7,71 @@ use Hillel\Models\Category;
 use Hillel\Models\Post;
 use Hillel\Models\Tag;
 
-$categories = [
-    [
-        'title' => 'PHP',
-        'slug' => 'Best language',
-    ],
-    [
-        'title' => 'HTML',
-        'slug' => 'Not a programming language'
-    ],
-    [
-        'title' => 'JawaScript',
-        'slug' => 'Everyone hate it'
-    ],
-    [
-        'title' => 'MySql',
-        'slug' => 'How to learn it'
-    ],
-    [
-        'title' => 'Go',
-        'slug' => 'The best name for programming language'
-    ]
-];
+//Create categories
+$categories = [];
 
-foreach ($categories as $category) {
-    Category::create($category);
+for ($i = 1; $i <= 5; $i++) {
+    $categories[] = [
+        'title' => 'Category' . $i,
+        'slug' => 'category-' . $i
+    ];
 }
 
-$category = Category::find(1);
+foreach ($categories as $category) {
+    \Hillel\Models\Category::create($category);
+}
+
+//Change random category
+$category = Category::inRandomOrder()->first();
 $category->title = 'Some new title';
 $category->save();
 
-$category = Category::find(1);
+//Delete random category
+$category = Category::inRandomOrder()->first();
 $category->delete();
 
-$posts = [
-    [
-        'title' => 'post1',
-        'slug' => 'post1',
-        'body' => 'post1',
-        'category_id' => '2'
-    ],
-    [
-        'title' => 'post2',
-        'slug' => 'post2',
-        'body' => 'post2',
-        'category_id' => '3'
-    ],
-    [
-        'title' => 'post3',
-        'slug' => 'post3',
-        'body' => 'post3',
-        'category_id' => '4'
-    ],
-     [
-        'title' => 'post4',
-        'slug' => 'post4',
-        'body' => 'post4',
-        'category_id' => '2'
-    ],
-    [
-        'title' => 'post2',
-        'slug' => 'post2',
-        'body' => 'post2',
-        'category_id' => '3'
-    ],
-    [
-        'title' => 'post5',
-        'slug' => 'post5',
-        'body' => 'post5',
-        'category_id' => '4'
-    ],
-];
+//Create posts
+$posts = [];
+$categories = \Hillel\Models\Category::all();
 
-foreach ($posts as $post) {
-    Post::create($post);
+for ($i = 1; $i <= 5; $i++) {
+    $posts[] = [
+        'title' => 'Post' . $i,
+        'slug' => 'post-' . $i,
+        'body' => 'Body of post ' . $i,
+        'category_id' => $categories->random()->id
+    ];
 }
 
-$post = Post::find(2);
+foreach ($posts as $post) {
+    \Hillel\Models\Post::create($post);
+}
+
+//Change random post
+$post = Post::inRandomOrder()->first();
 $post->title = 'New title';
-$post->category_id = 2;
+$post->category_id = $categories->random()->id;
 $post->save();
 
-$tags = [
-    [
-        'title' => 'tag1',
-        'slug' => 'slug1',
-    ],
-    [
-        'title' => 'tag2',
-        'slug' => 'slug2',
-    ],
-    [
-        'title' => 'tag3',
-        'slug' => 'slug3',
-    ],
-    [
-        'title' => 'tag4',
-        'slug' => 'slug4',
-    ],
-    [
-        'title' => 'tag5',
-        'slug' => 'slug5',
-    ]
-];
+//Create tags
+$tags = [];
 
-foreach ($tags as $tag) {
-    Tag::create($tag);
+for ($i = 1; $i <= 5; $i++) {
+    $tags[] = [
+        'title' => 'Tag' . $i,
+        'slug' => 'tag-' . $i
+    ];
 }
 
-$posts = Post::all();
+foreach ($tags as $tag) {
+    \Hillel\Models\Tag::create($tag);
+}
+
+//Assign 3 random tags for each post
+$tags = \Hillel\Models\Tag::all();
+$posts = \Hillel\Models\Post::all();
 
 foreach ($posts as $post) {
-    $post->tags()->attach(1);
-    $post->tags()->attach(2);
-    $post->tags()->attach(3);
-};
+    $tagsId = $tags->pluck('id')->random(3);
+    $post->tags()->sync($tagsId);
+}
--- src/Models/Category.php
+++ src/Models/Category.php
@@ -16,4 +16,15 @@ class Category extends Model
         return $this->hasMany(Post::class);
     }
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($category) {
+            $uncategorized = Category::firstOrCreate(['title' => 'Uncategorized', 'slug' =>
+                'uncategorized']);
+            Post::where('category_id', $category->id)->update(
+                ['category_id' => $uncategorized->id]);
+        });
+    }
 }
